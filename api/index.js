137e7c7508365aca1ef9f1417594e35d634a2e1d
codebonding <codebonding@gmail.com>
2024-11-22 10:19:41 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors()); 
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from Vercel!');
+});
+
+app.listen(5000, () => {
+    console.log('Server running on http://localhost:5000');
+  });
+
+module.exports = app;
